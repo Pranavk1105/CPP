6bc576fead5595ee42db4da9112df7e2198e5fd9
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-13 23:41:14 +0530

    Add files via upload

---
+++ fn_templ.cpp
@@ -0,0 +1,39 @@
+// Template to display Array
+#include <iostream>
+ using namespace std ;
+ template <class T>
+ void printArray(T arr[1] , int size) {
+    for (int i = 0;  i < size; i++) 
+    {
+        cout << arr[i]<<"";
+    }
+    cout << endl;
+ }
+
+ int main () 
+ {
+    int arr[] = { 1,2,3,4};
+    printArray(arr,4);   
+    return 0;
+ }
+
+
+
+// // template to display ARRAY
+// #include <iostream>
+// using namespace std;
+// template <class T>
+// void printArray(T arr[], int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << "";
+//     }
+//     cout << endl;
+// }
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4};
+//     printArray(arr, 4);
+//     return 0;
+// }
