54d2cc85ee65760ebb9124289154d4a707765768
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-19 22:18:22 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length = 10;
+    int breadth = 20;
+    int r = 5;
+    int Area_of_rectangle = length * breadth;
+    int Area_of_square = length * length;
+    int Area_of_circle = 3.14*r*r;
+    int Area_of_triangle = 0.5*length*breadth;
+
+}
